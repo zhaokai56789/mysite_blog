0d818c49489abe741a268c73fcbfd2b77c0fb0fc
zhaokai56789 <zhaokai56789@163.com>
2020-06-05 14:20:35 +0800

    初始化项目

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -33,5 +32,6 @@ urlpatterns = [
 ]
 
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
